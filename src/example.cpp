8af66fe1dfe79b1ea33b7065dc0ab05668459c2e
Ran Lu <rlu@wisc.edu>
2014-10-19 00:05:18 -0500

    Support more complicated input files

--- src/example.cpp
+++ src/example.cpp
@@ -4,20 +4,30 @@
 #include "JetFinder.h"
 #include "Debug.h"
 #include <iostream>
+#include <sstream>
 #include <vector>
 
 int main()
 {
     SlowJet::VectorList input_particles;
     double px, py , pz, E;
-    while (std::cin >> px >> py >> pz >> E) {
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        std::istringstream linestream(line);
+        // take substrings to avoid problems when there are extra "pollution"
+        // characters (e.g. line-feed).
+        if (line.substr(0,1) == "#") {continue;}
+        double px,py,pz,E;
+        linestream >> px >> py >> pz >> E;
         input_particles.push_back(SlowJet::Vector(px,py,pz,E));
     }
+#ifdef DEBUG
     for (auto pt : input_particles) {
         DEBUG_MSG("Input particles: " << pt.prettyOutput());
     }
+#endif
     DEBUG_MSG("================================================================================");
-    SlowJet::JetDefinition::instance()->setBeta(15.0);
+    SlowJet::JetDefinition::instance()->setBeta(12.0);
     SlowJet::JetFinder jf(input_particles);
     SlowJet::JetList jets = jf.jets();
     int count = 0;
