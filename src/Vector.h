5535ff1bb14deff8c980ed83d2e97a6406c4e948
Ran Lu <rlu@wisc.edu>
2014-11-24 15:03:05 -0600

    Remove superfluous const

--- src/Vector.h
+++ src/Vector.h
@@ -19,13 +19,13 @@ public:
     Vector() :m_p{{0,0,0,0}}, m_p_normalized{{0,0,0,0}}, m_associatedInners{}, m_associatedBoundaries{}, m_discarded(false), m_jetFunction(0) {}
     Vector(const double px, const double py, const double pz, const double E);
     const PArray & fourVector() const {return m_p;}
-    const double pt() const { return sqrt(m_p[0]*m_p[0]+m_p[1]*m_p[1]); }
-    const double eta() const {
+    double pt() const { return sqrt(m_p[0]*m_p[0]+m_p[1]*m_p[1]); }
+    double eta() const {
         double pAbs = sqrt(m_p[0]*m_p[0]+m_p[1]*m_p[1]+m_p[2]*m_p[2]);
         return 0.5*log((pAbs+m_p[2])/(pAbs-m_p[2]));
     }
-    const double y() const { return 0.5*log((m_p[3]+m_p[2])/(m_p[3]-m_p[2])); }
-    const double phi() const {
+    double y() const { return 0.5*log((m_p[3]+m_p[2])/(m_p[3]-m_p[2])); }
+    double phi() const {
         double arg = atan2(m_p[1],m_p[0]);
         if (arg < 0) {
             arg += 2*M_PI;
