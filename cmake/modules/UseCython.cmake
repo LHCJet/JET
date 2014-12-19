d9b4e7281b511467264291c8036527b5118b362c
Ran Lu <rlu@wisc.edu>
2014-12-19 01:13:40 -0600

    Make sure PythonLibs matches PythonInterp

--- cmake/modules/UseCython.cmake
+++ cmake/modules/UseCython.cmake
@@ -72,7 +72,8 @@ set( CYTHON_FLAGS "" CACHE STRING
 mark_as_advanced( CYTHON_ANNOTATE CYTHON_NO_DOCSTRINGS CYTHON_FLAGS )
 
 find_package( Cython REQUIRED )
-find_package( PythonLibs REQUIRED )
+find_package( PythonInterp REQUIRED)
+find_package( PythonLibs ${PYTHON_VERSION_STRING} EXACT REQUIRED )
 
 set( CYTHON_CXX_EXTENSION "cxx" )
 set( CYTHON_C_EXTENSION "c" )
